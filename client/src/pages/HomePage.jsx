2affe64eef39f859f289bd5bcb233d733ce853fa
HJayZ91 <huajie.zhuang@gmail.com>
2025-07-20 18:13:16 -0500

    added pagination

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,8 +1,14 @@
 import JobList from '../components/JobList';
 import { useJobs } from '../lib/graphql/hook';
+import { useState } from 'react';
+import PaginationBar from '../components/PaginationBar';
+
+const JOBS_PER_PAGE = 7;
 
 function HomePage() {
-  const { jobs, loading, error } = useJobs();
+  const [currentPage, setCurrentPage] = useState(1);
+  const { jobs, loading, error } = 
+    useJobs(JOBS_PER_PAGE, (currentPage - 1) * JOBS_PER_PAGE);
 
   console.log('[HomePage]', { jobs, loading, error });
   if (loading) {
@@ -11,12 +17,27 @@ function HomePage() {
   if (error) {
     return <div className="has-text-danger">Data unavailable</div>;
   }
+  const totalPages = Math.ceil(jobs.totalCount / JOBS_PER_PAGE);
   return (
     <div>
       <h1 className="title">
         Job Board
       </h1>
-      <JobList jobs={jobs} />
+      {/* <div>
+        <button disabled={currentPage === 1}
+          onClick={() => setCurrentPage(currentPage - 1)}>
+          Previous
+        </button>
+        <span> {`${currentPage} of ${totalPages}`} </span>
+        <button disabled={currentPage === totalPages}
+          onClick={() => setCurrentPage(currentPage + 1)}>
+          Next
+        </button>
+      </div> */}
+      <PaginationBar currentPage={currentPage} totalPages={totalPages}
+        onPageChange={setCurrentPage}
+      />
+      <JobList jobs={jobs.items} />
     </div>
   );
 }
